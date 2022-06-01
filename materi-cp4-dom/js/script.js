33ae488e4ba42bca8012377e78f850ed64ec9343
eL Setiyawan <mt-lilik.setiyawan@tech.jago.com>
2022-06-01 20:57:17 +0700

    materi chapter 4 - dom

---
+++ materi-cp4-dom/js/script.js
@@ -0,0 +1,42 @@
+// const pargraf1 = document.getElementById("p1");
+// pargraf1.innerText = "hei there";
+// pargraf1.style.backgroundColor = 'orange';
+
+// const pargraf2 = document.getElementsByName("p2");
+// pargraf2[0].style.backgroundColor = 'blue';
+// pargraf2[1].style.backgroundColor = 'yellow';
+
+// let button1 = document.getElementById("button1");
+// let paragraf = document.getElementById("p1");
+// let color;
+
+// button1.onclick = function () {
+//   alert("button 1 clicked");
+//   if (color === "yellow") {
+//     paragraf.style.backgroundColor = "white";
+//     color = 'white';
+//   } else {
+//     paragraf.style.backgroundColor = "yellow";
+//     color = 'yellow';
+//   }
+//   //   paragraf.style.backgroundColor = "yellow";
+// };
+
+// let button2 = document.getElementById("button2");
+// button2.addEventListener("click", () => {
+//   alert("button 2 clicked");
+//   paragraf.style.backgroundColor = "red";
+// });
+
+const button = document.getElementById("add");
+const paragraf1 = document.getElementById("p1");
+// let newElement = document.createElement("div");
+// newElement.innerText = "New Element";
+
+button.onclick = function () {
+  const parent = document.getElementById("wrapper");
+
+  let clonedElement = paragraf1.cloneNode(true);
+  parent.append(clonedElement);
+  //   let clonedElement = parent.append(newElement);
+};
