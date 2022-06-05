88c11ee79d655b4b7106bdffb989fa5e07c4b6e3
prayoga <prayoga2999@gmail.com>
2022-06-05 21:37:09 +0700

    Tic Tac Toe Simple, tanpa menang kalah dan manual

--- binar-tictactoe/js/script.js
+++ binar-tictactoe/js/script.js
@@ -1,27 +1,61 @@
-const cross = "cross";
-const circle = "circle";
+// const cross = "cross";
+// const circle = "circle";
 
-const boxElements = document.querySelectorAll(".box");
-const resetButton = document.getElementById("reset");
+// const boxElements = document.querySelectorAll(".box");
+// const resetButton = document.getElementById("reset");
 
-let turn = cross;
+// let turn = cross;
 
-resetButton.onclick = startGame;
+// resetButton.onclick = startGame;
 
-function placeMark(box, currentClass) {
-  box.classList.add(currentClass);
-}
+// function placeMark(box, currentClass) {
+//   box.classList.add(currentClass);
+// }
 
-function handleClick(e) {
-  const boxTarget = e.target;
+// function handleClick(e) {
+//   const boxTarget = e.target;
 
-  placeMark(boxTarget, turn);
-}
+//   placeMark(boxTarget, turn);
+// }
 
-function startGame() {
-  boxElements.forEach((box) => {
-    box.addEventListener("click", handleClick, { once: true });
+// function startGame() {
+//   boxElements.forEach((box) => {
+//     box.addEventListener("click", handleClick, { once: true });
+//   });
+// }
+
+// startGame();
+let box = document.querySelectorAll(".box");
+box = Array.from(box);
+
+let currentPlayer = "X";
+
+// let winningCombinations = [
+// [0,1,2],
+// [3,4,5],
+// [6,7,8],
+// [0,3,6],
+// [1,4,7],
+// [2,5,8],
+// [0,4,8],
+// [2,4,6]
+// ]
+
+// function checkForWinner(){
+//   winningCombinations.forEach(function(combination){
+//     let check = combination.every(idx => box [idx]innerText.trim() == currentPlayer)
+
+//   if(check){
+//     alert(currentPlayer + " Has Won")
+//   }
+//   })
+
+// }
+
+box.forEach(function (box) {
+  box.addEventListener("click", function () {
+    if (box.innerText.trim() != "") return;
+    box.innerText = currentPlayer;
+    currentPlayer = currentPlayer == "X" ? "O" : "X";
   });
-}
-
-startGame();
+});
